5c6f4836bff1165097c92fa049277642d76b1a5e
CHRIS-TECH <129108885+chrisniiemperor@users.noreply.github.com>
2026-07-31 17:42:34 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -4,9 +4,7 @@ import * as mega from 'megajs';
 const auth = {
     email: process.env.MEGA_EMAIL,
     password: process.env.MEGA_PASSWORD,
-    userAgent:
-        process.env.MEGA_USER_AGENT ||
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+    userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
 // Function to upload a file to Mega and return the URL
